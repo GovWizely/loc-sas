d8fd5ad2a8127f533ea276d096926099237a8b5c
lipsotiko <lipsotiko@gmail.com>
2019-12-31 14:58:49 -0500

    [RD-78] Users may view the content of an application that was submitted

--- client/src/main.js
+++ client/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import VueMaterial from 'vue-material'
 import VueCookies from 'vue-cookies'
+import JsonViewer from 'vue-json-viewer'
 import 'vue-material/dist/vue-material.min.css'
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
 Vue.use(VueCookies)
+Vue.use(JsonViewer)
 
 new Vue({
   router,
---
+++ client/src/views/CopyrightApplication.vue
@@ -0,0 +1,16 @@
+<template>
+  <json-viewer v-if="copyrightApplication" :value="copyrightApplication"></json-viewer>
+</template>
+<script>
+export default {
+  props: ['repository'],
+  data: () => {
+    return {
+      copyrightApplication: null
+    }
+  },
+  async created () {
+    this.copyrightApplication = await this.repository._getCopyrightApplication(this.$route.query['id'])
+  }
+}
+</script>
