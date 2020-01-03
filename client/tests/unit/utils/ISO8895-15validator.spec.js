83c8c3f24943adcf8805fd35de3792ac57ff4846
lipsotiko <lipsotiko@gmail.com>
2020-01-03 10:09:47 -0500

    Backfilling some tests for the ISO8895-15 formatter

---
+++ client/tests/unit/utils/ISO8895-15validator.spec.js
@@ -0,0 +1,12 @@
+import { replaceNonIso8895 } from '@/utils/ISO8895-15validator'
+import { expect } from 'chai'
+
+describe('ISO8895-15validator', () => {
+  it('removes non-ISO8895-15 characters from strings', () => {
+    expect(replaceNonIso8895('ABCΔ')).to.eq('ABC')
+  })
+
+  it('removes non-ISO8895-15 characters from strings', () => {
+    expect(replaceNonIso8895(true)).to.eq(true)
+  })
+})
