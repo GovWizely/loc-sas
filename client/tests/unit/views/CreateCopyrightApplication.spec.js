c221462b037725772a61343f2b098e75722b9233
James Isaacs <james@govwizely.com>
2020-01-09 15:28:44 -0500

    [RD-86] add certificate contact

--- client/tests/unit/views/CreateCopyrightApplication.spec.js
+++ client/tests/unit/views/CreateCopyrightApplication.spec.js
@@ -25,6 +25,14 @@ describe('Create Copyright Application', () => {
     wrapper.find('#claimant-state').setValue('VA')
     wrapper.find('#claimant-postal-code').setValue('12345')
     wrapper.find('#claimant-country').setValue('USA')
+
+    wrapper.find('#certificate-contact-first-name').setValue('George')
+    wrapper.find('#certificate-contact-last-name').setValue('Washington')
+    wrapper.find('#certificate-contact-address').setValue('1234 Cool Ln')
+    wrapper.find('#certificate-contact-city').setValue('Arlington')
+    wrapper.find('#certificate-contact-state').setValue('VA')
+    wrapper.find('#certificate-contact-postal-code').setValue('12345')
+    wrapper.find('#certificate-contact-country').setValue('USA')
     await wrapper.vm.$nextTick()
 
     wrapper.find('#copy-claimant-address-btn').trigger('click')
@@ -57,6 +65,14 @@ describe('Create Copyright Application', () => {
     expect(submittedApplication.claimantState).to.equal('VA')
     expect(submittedApplication.claimantPostalCode).to.equal('12345')
     expect(submittedApplication.claimantCountry).to.equal('USA')
+
+    expect(submittedApplication.certificateContactFirstName).to.equal('George')
+    expect(submittedApplication.certificateContactLastName).to.equal('Washington')
+    expect(submittedApplication.certificateContactAddress).to.equal('1234 Cool Ln')
+    expect(submittedApplication.certificateContactCity).to.equal('Arlington')
+    expect(submittedApplication.certificateContactState).to.equal('VA')
+    expect(submittedApplication.certificateContactPostalCode).to.equal('12345')
+    expect(submittedApplication.certificateContactCountry).to.equal('USA')
 
     expect(submittedApplication.correspondenceContactFirstName).to.equal('George')
     expect(submittedApplication.correspondenceContactLastName).to.equal('Washington')
