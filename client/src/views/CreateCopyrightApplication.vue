7ae0289e9c5e4967a5354fd33ff61bf6ad49f146
lipsotiko <lipsotiko@gmail.com>
2020-03-11 10:00:22 -0400

    [Rd-201] (fix) Allow for null publication dates

--- client/src/views/CreateCopyrightApplication.vue
+++ client/src/views/CreateCopyrightApplication.vue
@@ -695,7 +695,7 @@ export default {
           onOrBeforeToday: (publicationDate) => {
             return (!empty(publicationDate))
               ? moment(publicationDate, 'MMDDYYYY').isSameOrBefore(moment())
-              : false
+              : true
           }
         },
         authorOrganization: {
